b18cd9f0896e49bac6db3f1c0be9e3bf8274c587
sk8ersteve <stephen.bulldog.thomas3@gmail.com>
2018-02-10 09:59:51 -0500

    For ubuntu server

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import sys
-#import cv2
-#import numpy as np
+import cv2
+import numpy as np
 print (sys.version)
-print (sys.argv[1])
+#print (sys.argv[1])
 #F = open(sys.argv[1], 'r')
 #print (F.read())
-#img = cv2.imread(sys.argv[1])
-#for x in range (1, 100):
-#	print (str(x) + " " + str(img[x,x]))
+img = cv2.imread(sys.argv[1])
+for x in range (1, 100):
+	print (str(x) + " " + str(img[x,x]))
