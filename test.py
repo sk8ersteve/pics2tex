1ee4af9378e25157854e622b1d7d93919851f8fc
sk8ersteve <stephen.bulldog.thomas3@gmail.com>
2018-02-10 01:19:08 -0500

    Newest python test with opencv, python3

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print 'Hello'
+import sys
+import cv2
+import numpy as np
+print ('Hello ')
+#print (sys.argv[1])
+#F = open(sys.argv[1], 'r')
+#print (F.read())
+img = cv2.imread(sys.argv[1])
+for x in range (1, 100):
+	print (str(x) + " " + str(img[x,x]))
